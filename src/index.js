77a1e5afb0507e865ff29c4c988b86e7389e81be
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-07 16:23:17 -0600

    first commit, personal base styles implemented

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'unfetch/polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './contexts/UserContext'
+import App from './components/App/App'
+import './setup-icons'
+import './css/variables.css';
+import './css/styles.css';
+import * as serviceWorker from './serviceWorker'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+)
+
+serviceWorker.unregister()
