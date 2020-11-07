3503e8f7e79a27eccc651648f394b69dbac352ca
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-07 17:33:45 -0600

    changed white and implemented custom footer component

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -78,13 +78,13 @@ class RegistrationForm extends Component {
             required
           />
         </div>
-        <footer>
+        <div className='form_footer'>
           <Button type='submit'>
             Sign up
           </Button>
           {' '}
           <Link to='/login'>Already have an account?</Link>
-        </footer>
+        </div>
       </form>
     )
   }
